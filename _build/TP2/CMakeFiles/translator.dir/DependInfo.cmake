ad2c75bc5bfe9ce4361a6049385e59de3e6d7268
endouij <johndoe@example.com>
2023-02-21 10:52:02 +0100

    tp2

---
+++ _build/TP2/CMakeFiles/translator.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/2inf1/julien.debats/Documents/M1s2/CPP/CPP_Exercises/TP2/translator.cpp" "/home/2inf1/julien.debats/Documents/M1s2/CPP/CPP_Exercises/_build/TP2/CMakeFiles/translator.dir/translator.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
